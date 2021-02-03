5ad8b1bf53aaac2523847d82b658a7024f881dff
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-03 16:41:07 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
 import subprocess
-ch = input('Do you want to continue:\t')
-if ch == 'Y':
+ch = 'Y'
+while ch == 'Y':
 	path = input('Enter path of folder:\t')
 	subprocess.run(['ls',path])
-	
+	ch = input('Do you want to continue(Y/n):\t')
