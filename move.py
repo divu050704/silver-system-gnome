aec48b61a766109e38334e911cabcb3e717c66af
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-05 13:29:54 +0530

    Update move.py

--- move.py
+++ move.py
@@ -4,5 +4,7 @@ c = 'Y'
 while c == "Y":
 	path = input('Enter path from root to the file:\t')
 	dest = input('Enter destination of file:\t')
-	subprocess.run(['sudo','mv',path,dest])
+	y = input('Do you want to continue(Y/n):\t')
+	if y == 'Y':
+		subprocess.run(['sudo','mv',path,dest])
 	c = input('Do you want to continue moving files(Y/n):\t')
