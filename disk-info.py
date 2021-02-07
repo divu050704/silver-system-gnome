05e9baa1caa76f2e0588e590413488f1a3cbcbbf
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-07 17:51:21 +0530

    Update disk-info.py

--- disk-info.py
+++ disk-info.py
@@ -2,9 +2,12 @@ import os
 ch = 'Y'
 while ch == 'Y':
 	print('In which format you want information')
+	print('0.\t Exit')
 	print('1.\t Megabyte')
 	print('2.\t Bytes')
 	y = int(input('Enter your choice:\t'))
+	if y == 0:
+		exit()
 	if y == 1:
 		os.system('df -m')
 	if y == 2:
