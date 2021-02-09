80e81c4e7269ba5e410a7b1a60346864cd5b3017
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-09 09:28:47 +0530

    Add files via upload

---
+++ history.py
@@ -0,0 +1,11 @@
+import os
+ch = 'Y'
+while ch == 'Y':
+	print('1.\t Exit')
+	print('2.\t Want to see last 10 commands')
+	print('3.\t Want to see last 25 commands')
+	y = int(input('Enter your choice:\t'))
+	if y == 1:
+		
+		
+	
