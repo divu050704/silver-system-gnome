bea496fc292ff89d335a15bd37d594e392029c2c
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-10 10:23:54 +0530

    Update del.py

--- del.py
+++ del.py
@@ -1,38 +1,54 @@
 import os
 import subprocess
-print('Is it a file or a folder???')
-print('0.\t Exit')
-print('1.\t File')
-print('2.\t Folder')
+class color():
+   purple = '\033[95m'
+   cyan = '\033[96m'
+   darkcyan = '\033[36m'
+   blue = '\033[94m'
+   blued = '\033[92m'
+   yellow = '\033[93m'
+   red = '\033[91m'
+   silver = '\033[3;30;47m'
+   orange= '\033[31;43m'
+   bold = '\033[1m'
+   green = '\033[32m'
+   underline = '\033[4m'
+   end = '\033[0m'
+
 y= 'Y'
 while y == 'Y':
-	ch = int(input('Enter your choice:\t'))
+	print(color.yellow+'Is it a file or a folder???'+color.end)
+	print(color.red+'0.\t'+color.green+'Exit'+color.end)
+	print(color.red+'1.\t'+color.green+'File'+color.end)
+	print(color.red+'2.\t'+color.green+'Folder'+color.end)
+	ch = int(input(color.purple+'Enter your choice:\t'+color.end))
 	if ch == 0:
+		print(color.cyan+'Exiting........'+color.end)
 		exit()
 	if ch == 1:
 		c = 'Y'
 		while c == 'Y':
-			path = input('Enter path to file:\t')
-			print('Files in ',path,':-')
+			path = input(color.darkcyan+'Enter path to file:\t'+color.end)
+			print(color.purple+'Files in '+color.end,path,':-')
 			os.chdir(path)
 			subprocess.run(['ls'])
-			c1 = input('Do you want to continue removing file(Y/n):\t')
+			c1 = input(color.darkcyan+'Do you want to continue removing file(Y/n):\t'+color.end)
 			if c1 == 'Y':
 				os.chdir(path)
-				file1 = input('Enter file name:\t')
+				file1 = input(color.yellow+'Enter file name:\t'+color.end)
 				subprocess.run(['sudo','rm','file1'])
-			c = input('Do you want to continue removing files(Y/n):\t')
+			c = input(color.purple+'Do you want to continue removing files(Y/n):\t'+color.end)
 	if ch == 2:
 		c2 = 'Y'
 		while c2 == 'Y':
-			path1 = input('Enter path to folder:\t')
-			print('Folders an files in',path1,':-')
+			path1 = input(color.darkcyan+'Enter path to folder:\t'+color.end)
+			print(color.purple+'Folders an files in'+color.end,path1,':-')
 			os.chdir(path1)
 			os.system('ls')
-			c3 = input('Do you want to continue(Y/n):\t') 
+			c3 = input(color.darkcyan+'Do you want to continue(Y/n):\t'+color.end) 
 			if c3 == 'Y':
 				os.chdir(path1)
-				folder = input('Enter folder name:\t')
+				folder = input(color.yellow+'Enter folder name:\t'+color.end)
 				subprocess.run(['sudo','rm','-r',folder])
-			c2 = input('Do you want to continue removing folder(Y/n):\t')					
-	y = input('Do you want to continue removing files and folders(Y/n):\t')
+			c2 = input(color.purple+'Do you want to continue removing folder(Y/n):\t'+color.end)					
+	y = input(color.orange+color.bold+'Do you want to continue removing files and folders(Y/n):\t'+color.end)
