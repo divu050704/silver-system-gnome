87e85345117dbf95a7729b46c92386970fedede5
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-10 13:42:49 +0530

    Update move.py

--- move.py
+++ move.py
@@ -1,10 +1,26 @@
 import os 
 import subprocess
+class color:
+   purple = '\033[95m'
+   cyan = '\033[96m'
+   darkcyan = '\033[36m'
+   blue = '\033[94m'
+   blued = '\033[92m'
+   yellow = '\033[93m'
+   red = '\033[91m'
+   silver = '\033[1;30;40m'
+   orange = '\033[33m'
+   green = '\033[32m'
+   bold = '\033[1m'
+   underline = '\033[4m'
+   end = '\033[0m'
 c = 'Y'
 while c == "Y":
-	path = input('Enter path from root to the file:\t')
-	dest = input('Enter destination of file:\t')
-	y = input('Do you want to continue(Y/n):\t')
-	if y == 'Y':
-		subprocess.run(['sudo','mv',path,dest])
-	c = input('Do you want to continue moving files(Y/n):\t')
+	path = input(color.purple+'Enter path from root to the file:\t'+color.end)
+	subprocess.run(['ls',path])
+	file1 = input(color.green+'Enter file name with full directories:\t'+color.end)
+	dest = input(color.purple+'Enter destination of file:\t'+color.end)
+	subprocess.run(['ls',dest])
+	file2 = input(color.green+'Enter file name with full directories:\t'+color.end)
+	subprocess.run(['sudo','mv',file1,file2])
+	c = input(color.bold+color.blued+'Do you want to continue moving files(Y/n):\t'+color.end)
