c5a91ca22ce7d4af325aa24e92cd560fee0d9d97
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-10 13:41:40 +0530

    Update knowledge.py

--- knowledge.py
+++ knowledge.py
@@ -1,208 +1,205 @@
 c = 'Y'
+class color():
+   purple = '\033[95m'
+   cyan = '\033[96m'
+   darkcyan = '\033[36m'
+   blue = '\033[94m'
+   blued = '\033[92m'
+   yellow = '\033[93m'
+   red = '\033[91m'
+   silver = '\033[3;30;47m'
+   orange= '\033[31;43m'
+   bold = '\033[1m'
+   green = '\033[32m'
+   underline = '\033[4m'
+   end = '\033[0m'
+
 while c == 'Y':
-	print('0.\t Exit')
-	print('1.\t See boot time')
-	print('2.\t System upgrade')
-	print('3.\t Ping any website')
-	print('4.\t Search devices connected to wifi')
-	print('5.\t Install new application')
-	print('6.\t Run an application')
-	print('7.\t Remove an application')
-	print('8.\t Change your working directory')
-	print('9.\t To see files in a particular folder')
-	print('10.\t To remove files and folders')
-	print('11.\t Move files and folders')
-	print('12.\t Copy files and folders')
-	print('13.\t Locate files and folders')
-	print('14.\t Search a keyword in a file')
-	print('15.\t Want to know about your machine')
-	print('16.\t Want to see disk information')
-	ch = int(input('Enter your choice:\t'))
+	print(color.red+'1.\t'+color.green+'See boot time'+color.end)
+	print(color.red+'2.\t'+color.green+'Do a system upgrade'+color.end)
+	print(color.red+'3.\t'+color.green+'Ping any website'+color.end)
+	print(color.red+'4.\t'+color.green+'Search devices connected to wifi'+color.end)
+	print(color.red+'5.\t'+color.green+'Install a new application'+color.end)
+	print(color.red+'6.\t'+color.green+'Run an application'+color.end)
+	print(color.red+'7.\t'+color.green+'Remove an application'+color.end)
+	print(color.red+'8.\t'+color.green+'Change your working directory'+color.end)
+	print(color.red+'9.\t'+color.green+'Want to see files in a particular folder'+color.end)
+	print(color.red+'10.\t'+color.green+'Want to remove files and folders'+color.end)
+	print(color.red+'11.\t'+color.green+'Move files and folders'+color.end)
+	print(color.red+'12.\t'+color.green+'Copy files and folders'+color.end)
+	print(color.red+'13.\t'+color.green+'Locate files and folders'+color.end)
+	print(color.red+'14.\t'+color.green+'Search a keyword in a file'+color.end)
+	print(color.red+'15.\t'+color.green+'Want to know about your machine'+color.end)
+	print(color.red+'16.\t'+color.green+'Want to see disk information'+color.end)
+	ch = float(input(color.purple+'Enter you choice:\t'+color.end))	
 	if ch == 0:
 		exit()
 	if ch == 1:
-		print('\nCode is:\t','$ systemd-analyze')	
-		print('\nFor blame:\t','$ systemd-analyze blame')
-		print('\nFor chain:\t','$ systemd-analyze blame-chain')
+		print(color.yellow+'\nCode is:\t',color.blued+'$ systemd-analyze'+color.end)	
+		print(color.yellow+'\nFor blame:\t',color.blued+'$ systemd-analyze blame'+color.end)
+		print(color.yellow+'\nFor cahin:\t',color.blued+'$ systemd-analyze critical-chain'+color.end)
 	if ch == 2:
-		print('\n$ sudo apt update && sudo apt lsit --upgradable -a')
-		print('If upgrade:- $ sudo apt upgrade')
+		print(color.blued+'\n$ sudo apt update && sudo apt lsit --upgradable -a'+color.end)
+		print(color.yellow+'If upgrade:-\t'+color.blued+'$ sudo apt upgrade'+color.end)
 	if ch == 3:
-		print('\n$ ping <website>')
+		print(color.blued+'\n$ ping <website>'+color.end)
 	if ch == 4:
-		print('\n$ sudo nmap -sn <ip-address of router>')
-		print('ping <ip of device>')
+		print(color.blued+'\n$ sudo nmap -sn <ip-address of router>'+color.end)
+		print(color.blued+'ping <ip of device>'+color.end)
 	if ch == 5:
 		p = 'Y'
 		while p == 'Y':
-			print('0.\t Exit')
-			print('1.\t From apt')
-			print('2.\t From snap')
-			print('3.\t From software center')
-			y = int(input('Enter you choice:\t'))
+			print(color.red+'0.\t'+color.green+'Exit'+color.end)
+			print(color.red+'1.\t'+color.green+'Install from apt'+color.end)
+			print(color.red+'2.\t'+color.green+'Install from snap'+color.end)
+			print(color.red+'3.\t'+color.green+'Install from git'+color.end)
+			print(color.red+'4.\t'+color.green+'Instll from software center'+color.end)
+			y = int(input(color.purple+'Enter choice:\t'+color.end))
 			if y == 0:
 				exit()
 			if y == 1:
-				print('\nFirst:- $ sudo apt search <software>')
-				print('Then:- $ sudo apt install <software>')
+				print(color.yellow+'\nFirst:-\t'+color.blued+'$ sudo apt search <software>')
+				print(color.yellow+'Then:-\t'+color.blued+'$ sudo apt install <software>')
 			if y == 2:
-				print('\nFirst:- $ sudo snap search <software>')
-				print('Then:- $ sudo snap install <software>')
+				print(color.yellow+'\nFirst:-\t'+color.blued+'$ sudo snap search <software>')
+				print(color.blued+'Then:- $ sudo snap install <software>')
 			if y == 3:
 				print('\n$ gnome-software')
-			p = input('\nDo you want to continue search code for install(Y/n):\t')
+			p = input(color.cyan+'\nDo you want to continue search code for install(Y/n):\t'+color.end)
 	if ch == 6:
 		c2 = 'Y'
 		while c2 == 'Y':
-			print('0.\t Exit')		
-			print('1.\t Audacity')
-			print('2.\t Blender')
-			print('3.\t Calculator')
-			print('4.\t Cheese')
-			print('5.\t Files')
-			print('6.\t Gedit text editor')
-			print('7.\t Google Chrome')
-			print('8.\t htop')
-			print('9.\t Idle')
-			print('10.\t LibreOffice')
-			print('11.\t Microsoft Edge')
-			print('12.\t Microsoft teams')
-			print('13.\t Mozilla Firefox')
-			print('14.\t Openshot')
-			print('15.\t Rhythmbox')
-			print('16.\t Settings')
-			print('17.\t Smplayer')
-			print('18.\t Telegram')
-			print('19.\t Terminal')
-			print('20.\t Tweak')
-			print('21.\t VLC Player')
-			print('22.\t Vim')
-			print('23.\t Virtual Machines')
-			print('24.\t Whatsapp')
-			y1 = int(input('Enter you choice:\t'))
+			print(color.yellow+'Which application You want to open:'+color.end)
+			print(color.red+'0.\t'+color.green+'Exit'+color.end)
+			print(color.red+'1.\t'+color.green+'Audacity'+color.end)	
+			print(color.red+'2.\t'+color.green+'Blender'+color.end)
+			print(color.red+'3.\t'+color.green+'Calculator'+color.end)
+			print(color.red+'4.\t'+color.green+'Cheese'+color.end)
+			print(color.red+'5.\t'+color.green+'Files'+color.end)
+			print(color.red+'6.\t'+color.green+'Gedit text editor'+color.end)
+			print(color.red+'7.\t'+color.green+'Google Chrome'+color.end)
+			print(color.red+'8.\t'+color.green+'htop'+color.end)
+			print(color.red+'9.\t'+color.green+'Idle '+color.end)
+			print(color.red+'10.\t'+color.green+'Libreoffice'+color.end)
+			print(color.red+'11.\t'+color.green+'Microsoft Edge'+color.end)
+			print(color.red+'12.\t'+color.green+'Microsoft Teams'+color.end)
+			print(color.red+'13.\t'+color.green+'Mozilla Firefox'+color.end)
+			print(color.red+'14.\t'+color.green+'Openshot'+color.end)
+			print(color.red+'15.\t'+color.green+'Rhythmbox'+color.end)
+			print(color.red+'16.\t'+color.green+'Settings'+color.end)
+			print(color.red+'17.\t'+color.green+'Smplayer'+color.end)
+			print(color.red+'18.\t'+color.green+'Telegram'+color.end)
+			print(color.red+'19.\t'+color.green+'Terminal'+color.end)
+			print(color.red+'20.\t'+color.green+'Tweak your ubuntu'+color.end)	
+			print(color.red+'21.\t'+color.green+'VLC Player'+color.end)
+			print(color.red+'22.\t'+color.green+'Vim'+color.end)
+			print(color.red+'23.\t'+color.green+'Virtual Machines'+color.end)
+			print(color.red+'24.\t'+color.green+'Whatsapp'+color.end)
+			y1 = int(input(color.purple+'Enter you Choice:\t'+color.end))
 			if y1 == 1:
-				print('\n$ auadcity')			
+				print(color.blued+'\n$ auadcity'+color.end)			
 			if y1 == 2:
-				print('\n$ blender')
+				print(color.blued+'\n$ blender'+color.end)
 			if y1 == 3:
-				print('\n$ gnome-calculator')
+				print(color.blued+'\n$ gnome-calculator'+color.end)
 			if y1 == 4:
-				print('\n$ cheese')
+				print(color.blued+'\n$ cheese'+color.end)
 			if y1 == 5:
-				print('\n$ nautilus')
+				print(color.blued+'\n$ nautilus'+color.end)
 			if y1 == 6:
-				print('\n$ gedit <path-to-file>')
+				print(color.blued+'\n$ gedit <path-to-file>'+color.end)
 			if y1 == 7:
-				print('\n$ google-chrome')
+				print(color.blued+'\n$ google-chrome'+color.end)
 			if y1 == 8:
-				print('\n$ htop')
+				print(color.blued+'\n$ htop'+color.end)
 			if y1 == 9:
-				print('\n$ idle <path-to-file>')
+				print(color.blued+'\n$ idle <path-to-file>'+color.end)
 			if y1 == 10:
 				y3 = 'Y'
 				while y3 == 'Y':
-					print('0.\t Exit')
-					print('1.\t Documents')
-					print('2.\t Excel')
-					print('3.\t Powerpoint')
-					print('4.\t One note')
-					print('5.\t Libreoffice Math')
-					ch1 = int(input('Enter your '))
+					print(color.red+'0.\t'+color.green+'Exit'+color.end)
+					print(color.red+'1.\t'+color.green+'Documents'+color.end)
+					print(color.red+'2.\t'+color.green+'Excel'+color.end)
+					print(color.red+'3.\t'+color.green+'Powerpoint'+color.end)
+					print(color.red+'4.\t'+color.green+'One note'+color.end)
+					print(color.red+'5.\t'+color.green+'Libreoffice Math'+color.end)
+					ch1  = int(input(color.purple+'Enter your choice:\t'+color.end))
 					if ch1 == 0:
 						exit()
 					if ch1 == 1:
-						print('\n$ libreoffice --writer') 
+						print(color.blued+'\n$ libreoffice --writer'+color.end) 
 					if ch1 == 2:
-						print('\n$ libreoffice --calc')
+						print(color.blued+'\n$ libreoffice --calc'+color.end)
 					if ch1 == 3:
-						print('\n$ libreoffice --impress')
+						print(color.blued+'\n$ libreoffice --impress'+color.end)
 					if ch1 == 4:
-						print('\n$ libreoffice --draw')
+						print(color.blued+'\n$ libreoffice --draw'+color.end)
 					if ch1 == 5:
-						print('\n$ libreoffice --math')
-					y3 = input('\nDo you want to continue opening LibreOffice(Y/n):\t')
+						print(color.blued+'\n$ libreoffice --math'+color.end)
+					y3 = input(color.purple+'\nDo you want to continue opening LibreOffice(Y/n):\t'+color.end)
 			if y1 == 11:
-				print('\n$ microsoft-edge')
+				print(color.blued+'\n$ microsoft-edge'+color.end)
 			if y1 == 12:
-				print('\n$ teams')
+				print(color.blued+'\n$ teams'+color.end)
 			if y1 == 13:
-				print('\n$ firefox')
+				print(color.blued+'\n$ firefox'+color.end)
 			if y1 == 14:
-				print('\n$ openshot-qt')
+				print(color.blued+'\n$ openshot-qt'+color.end)
 			if y1 == 15:
-				print('\n$ rhythmbox')
+				print(color.blued+'\n$ rhythmbox'+color.end)
 			if y1 == 16:
-				print('\n$ gnome-control-center')
+				print(color.blued+'\n$ gnome-control-center'+color.end)
 			if y1 == 17:
-				print('\n$ smplayer')
+				print(color.blued+'\n$ smplayer'+color.end)
 			if y1 == 18:
-				print('\n$ telegram-desktop')
+				print(color.blued+'\n$ telegram-desktop'+color.end)
 			if y1 == 19:
-				print('\n$ gnome-terminal')
+				print(color.blued+'\n$ gnome-terminal'+color.end)
 			if y1 == 20:
-				print('\n$ gnome-tweaks')
+				print(color.blued+'\n$ gnome-tweaks'+color.end)
 			if y1 == 21:
-				print('\n$ /usr/bin/vlc')
+				print(color.blued+'\n$ /usr/bin/vlc'+color.end)
 			if y1 == 22:
-				print('\n$ vim <path><file-name>')	
+				print(color.blued+'\n$ vim <path><file-name>'+color.end)	
 			if y1 == 23:
-				print('\n$ VirtualBoxVM --startvm <name-of-virtual-machine>')
+				print(color.blued+'\n$ VirtualBoxVM --startvm <name-of-virtual-machine>'+color.end)
 			if y1 == 24:
-				print('\n$ google-chrome --app https://web.whatsapp.com')
-			c2 = input('\nDo you want to continue serching codes for applications(Y/n):\t')
+				print(color.blued+'\n$ google-chrome --app https://web.whatsapp.com'+color.end)
+			c2 = input(color.purple+'\nDo you want to continue serching codes for applications(Y/n):\t'+color.end)
 	if ch == 7:
 		i = 'Y'
 		while i == 'Y':
 			print('Software:-')
-			print('0.\t Exit')
-			print('1.\t apt')
-			print('2.\t snap')
-			g = int(input('Enter your choics:\t'))
+			print(color.red+'0.\t'+color.green+'Exit'+color.end)
+			print(color.red+'1.\t'+color.green+'Install from apt'+color.end)
+			print(color.red+'2.\t'+color.green+'Install from snap'+color.end)
+			print(color.red+'3.\t'+color.green+'Install from git'+color.end)
+			print(color.red+'4.\t'+color.green+'Instll from software center'+color.end)
+			g = int(input(color.purple+'Enter your choics:\t'+color.end))
 			if g == 0:
+				print(color.cyan+'Exiting...............'+color.end)
 				exit()
 			if g == 1:
-				print('\n$ sudo apt remove <software>')
-				print('if autoremove performed:\t$ sudo apt autoremove')
+				print(color.blued+'\n$ sudo apt remove <software>'+color.end)
+				print(color.yellow+'If autoremove performed:\t'+color.blued+'$ sudo apt autoremove'+color.end)
 			if g == 2:
-				print('\n$ sudo snap remove <software>')				
-				print('If autoremove performed:\t$ sudo apt autoremove')
-			i = input('\nDo you want to continue see code for remove of software:\t')
+				print(color.blued+'\n$ sudo snap remove <software>'+color.end)				
+				print(color.yellow+'If autoremove performed:\t'+color.blued+'$ sudo apt autoremove'+color.end)
+			i = input(color.purple+'\nDo you want to continue see code for remove of software:\t'+color.end)
 	if ch == 8:
-		print('\n$ cd <path>')
+		print(color.blued+'\n$ cd <path>'+color.end)
 	if ch == 9:
-		print('\n$ ls <path-to-directory>')
+		print(color.blued+'\n$ ls <path-to-directory>'+color.end)
 	if ch == 10:
 		k = 'Y'
 		while k == 'Y':
-			print('Type:-')
-			print('1.\t File' )
-			print('2.\t Folder')
-			d = int(input('Enter your choice:\t'))
+			print(color.yellow+'Type:-'+color.end)
+			print(color.red+'1.\t'+color.green+'File' +color.end)
+			print(color.red+'2.\t'+color.green+'Folder'+color.end)
+			d = int(input(color.purple+'Enter your choice:\t'+color.end))
 			if d == 1:
-				print('\n$ sudo rm <path><file>')
+				print(color.blue+'\n$ sudo rm <path><file>'+color.end)
 			if d == 2:
-				print('\n$ sudo rm -r <path><folder>')
-			k = input('\nDo you want to continue see code for remove of files and folders(Y/n):\t')
-	if ch == 11:
-		print('\n$ mv <file-to-be-moved> <final-destination-of-file-or-folder>')
-	if ch == 12:
-		print('\n$ cp <file-to-be-copied> <final-destination-of-file-or-folder>')
-	if ch == 13:
-		print('\n$ locate <file-name>')
-	if ch == 14:
-		print('\n$ grep <keyword> <file-name>')
-	if ch == 15:
-		print('\n$ neofetch')
-	if ch == 16:
-		t = 'Y'
-		while t == 'Y':
-			print('In which format you want')
-			print('1.\t megabytes')
-			print('2.\t bytes')
-			d = int(input('Enter your choice:\t'))
-			if d == 1:
-				print('$ df -m')
-			if d == 2:
-				print('$ df')
-			t = input('Do you want to continue searching code for about disk:\t')
-	c = input('\nDo you want to continue looking for codes(Y/n):\t') 
+				print(color.blue+'\n$ sudo rm -r <path><folder>'+color.end)
+			k = input(color.purple+'\nDo you want to continue see code for remove of files and folders(Y/n):\t'+color.end)
+	c = input(color.darkcyan+color.bold+'\nDo you want to continue looking for codes(Y/n):\t'+color.end) 
