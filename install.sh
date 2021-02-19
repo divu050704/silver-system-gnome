2f64960879a32cd1405a3aa507e52dc291ef6d55
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-19 13:48:40 +0530

    New installation file

---
+++ install.sh
@@ -0,0 +1,2 @@
+echo -e "\e[1;35mInstalling...... \e[0m"
+sudo mv programs /usr/share/applications/.programs && chmod +x /usr/share/applications/.programs/main.sh && sudo mv silver-system.desktop /usr/share/applications
