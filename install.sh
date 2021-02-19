bdc4a0aab8f93ec06bdbd1d569001cc02e2b8fdf
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-19 18:39:00 +0530

    removed a bug

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 echo -e "\e[1;35mInstalling...... \e[0m"
-sudo mv .programs /usr/.silver-system-programs && chmod +x /usr/.silver-system-programs/main.sh && sudo mv silver-system.desktop /usr/share/applications
+sudo mv .silver-system-programs /usr/.silver-system-programs && chmod +x /usr/.silver-system-programs/main.sh && sudo mv silver-system.desktop /usr/share/applications
 echo -e "\e[1;31mInstalled\e[0m"
