246828003e835ae09ba00b25ccd4bd6f6c4057ad
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-21 20:53:23 +0530

    Updated for desktop shortcut also

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 echo -e "\e[1;35mInstalling...... \e[0m"
-sudo mv .silver-system-programs /usr/.silver-system-programs && chmod +x /usr/.silver-system-programs/main.sh && sudo mv silver-system.desktop /usr/share/applications
+sudo mv .silver-system-programs /usr/.silver-system-programs && chmod +x /usr/.silver-system-programs/main.sh && sudo mv silver-system.desktop /usr/share/applications && cp /usr/share/applications/silver-system.desktop ~/Desktop
 echo -e "\e[1;31mInstalled\e[0m"
