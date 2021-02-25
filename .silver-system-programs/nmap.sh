9cf4b41c8bc6c74b796282cf83cc821ba7c34b1a
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-25 13:12:27 +0530

    Improved the code and it can now access internet router of any bits

--- .silver-system-programs/nmap.sh
+++ .silver-system-programs/nmap.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 ip=$(hostname -I)
 nip=$(echo "$ip" | xargs)
-nnip=${nip}/24
+cla=$(ip a | grep $nip)
+read string1 string2 string3 <<< "$cla"
 folder=/usr/share/nmap
 if [ -d $folder ] 
 then
-    sudo nmap -sn $nnip
+    sudo nmap -sn $string2
 else
     sudo apt install nmap 
-    sudo nmap -sn $nnip
+    sudo nmap -sn $string2
 fi
--- .silver-system-programs/wifi.py
+++ .silver-system-programs/wifi.py
@@ -1,4 +1,2 @@
 import os
 os.system('chmod +x nmap.sh && ./nmap.sh')
-
-	
