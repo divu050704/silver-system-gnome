41954e02bf6e50ffcd421adc0854ca1dfb874592
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-05-10 13:35:40 +0530

    Now you don't have to write your ip address to see the users of your wifi and using wireshark database for better result

---
+++ .silver-system-programs/nmap.py
@@ -0,0 +1,13 @@
+file = open('/usr/.silver-system-programs/new.txt')
+main=file.read()
+words=main.split()
+final = len(words)
+file1 = open('/usr/.culprit/manuf.txt')
+main1 = file1.readlines()
+final1 = len(main1)
+for i in range(final):
+    for j in range(final1):
+        if words[i] in main1[j]:
+            print(main1[j])
+        
+
